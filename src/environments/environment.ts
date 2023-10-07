8f0852f9d303fa08a22df3555adef6d4b114b558
kev <kvnapg@gmail.com>
2023-10-07 18:26:25 -0500

    Install firebase

---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    projectId: 'save-money-3683e',
+    appId: '1:952838929754:web:6f998bdb93ff0107678763',
+    storageBucket: 'save-money-3683e.appspot.com',
+    apiKey: 'AIzaSyCcxRXaWzyenDaeneYpkCaBuMxAbL7iUkg',
+    authDomain: 'save-money-3683e.firebaseapp.com',
+    messagingSenderId: '952838929754',
+  },
+};
