09109424cd90d0eeab460084b15b1967f2c1e7bb
kev <kvnapg@gmail.com>
2023-10-07 18:27:30 -0500

    Create auth module and login component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'save-money';
+
+  ngOnInit(): void {
+    const totalDias = 183;
+    const arrayDeDias = Array.from({ length: totalDias }, (_, index) => index + 1);
+
+    // Multiplicar cada elemento por 1000 para obtener el valor en miles
+    const arrayValoresMonetarios = arrayDeDias.map((valor) => valor * 1000);
+
+    // Sumar los valores en el array
+    const sumaTotal = arrayValoresMonetarios.reduce((acumulador, valorActual) => acumulador + valorActual, 0);
+
+    console.log("El valor total en miles es:", sumaTotal);
+
+
+  }
 }
