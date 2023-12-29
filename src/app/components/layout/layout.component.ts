85c65c4aec90ffa662284e9a39ffba2a82514838
kev <kvnapg@gmail.com>
2023-12-29 11:04:59 -0500

    Show or hide sidenav

--- src/app/components/layout/layout.component.ts
+++ src/app/components/layout/layout.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { BreakpointService } from 'src/app/services/breakpoint/breakpoint.service';
 
 @Component({
   selector: 'app-layout',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./layout.component.scss']
 })
 export class LayoutComponent {
+
+  isSidenav = true;
+
+  constructor(
+    private readonly _breakpointService: BreakpointService
+  ) { }
+
+  ngOnInit() {
+    this._breakpointService.resizeScreen()
+      .subscribe((matches: boolean) => this.isSidenav = matches);
+  }
 
 }
